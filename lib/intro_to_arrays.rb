643693ff7a037b13106d71c4964a4907271fdcf3
Francis Mendez <fmendez884@gmail.com>
2019-04-10 12:57:05 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  return 
+end
+
+def array_with_two_elements
+  
+end
+
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
+
+def length_of_array
+  
+end
